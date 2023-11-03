845e3824c4cea93d9bd3ade8e35e96b553b314f0
Nemoto Masaharu <nemoto.masaharu.it@gmail.com>
2023-11-03 16:44:19 +0900

    実装

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,1 +1,16 @@
-export default function newRelicLogApiTransport(): Promise<void>;
+/// <reference types="node" />
+import build from 'pino-abstract-transport';
+export declare const endpointEnum: {
+    eu: string;
+    us: string;
+    fedRAMP: string;
+};
+type EndpointEnum = typeof endpointEnum;
+type Endpoint = EndpointEnum[keyof EndpointEnum];
+export type NewrelicLogAPITransportOption = {
+    licenseKey?: string;
+    endpoint: Endpoint | string;
+    headerLessAuth?: boolean;
+};
+export default function newRelicTransport({ licenseKey, endpoint, headerLessAuth, }: NewrelicLogAPITransportOption): Promise<import("stream").Transform & build.OnUnknown>;
+export {};
